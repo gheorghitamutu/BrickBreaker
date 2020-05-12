c13ed6f95983d28cf67fbf830f15df621e1c766d
Gheorghita Mutu <gmutu@bitdefender.com>
2020-05-12 15:45:07 +0300

    + added example events - removed flags for windows 10

--- app/Main.hs
+++ app/Main.hs
@@ -5,9 +5,11 @@ import SDL
 import SDL.Vect (V2(..), V4(..))
 import Control.Monad (unless)
 import Foreign.C.Types (CInt)
+import Prelude hiding (Left, Right)
 
 -- mine
 import qualified Colours (white, black)
+import qualified Structures(Intent, Direction, SurfaceMap, surfacePaths, selectSurface)
 
 screenWidth, screenHeight :: CInt
 (screenWidth, screenHeight) = (1280, 720)
@@ -16,10 +18,18 @@ main :: IO ()
 main = do
   SDL.initializeAll
   
-  window <- createWindow "BrickBreaker" defaultWindow { SDL.windowInitialSize = V2 screenWidth screenHeight }
-  renderer <- createRenderer window (-1) defaultRenderer
+  screen <- SDL.getWindowSurface w
+      surfaces <- mapM SDL.loadBMP surfacePaths
   
-  appLoop renderer True
+      let doRender = C.renderSurfaceToWindow w screen
+      doRender (help surfaces)
+  
+      whileM $
+        mkIntent <$> SDL.pollEvent
+        >>= runIntent surfaces doRender
+  
+      mapM_ SDL.freeSurface surfaces
+      SDL.freeSurface screen
 
 appLoop :: Renderer -> Bool -> IO ()
 appLoop renderer switch = do
@@ -45,7 +55,48 @@ appLoop renderer switch = do
 
   rendererDrawColor renderer $= backgroundColor
 
+  render renderer
+
   clear renderer
   present renderer
 
   unless exitWanted (appLoop renderer maybeNotSwitch)
+
+render :: SDL.Renderer -> IO ()
+render renderer = do
+  SDL.clear renderer
+  SDL.drawRect renderer (Just $ SDL.Rectangle (P $ V2 380 280) (V2 40 40))
+  SDL.present renderer
+
+mkIntent :: Maybe SDL.Event -> Intent
+mkIntent = maybe Idle (payloadToIntent . extractPayload)
+
+extractPayload :: SDL.Event -> SDL.EventPayload
+extractPayload (SDL.Event _t p) = p
+
+payloadToIntent :: SDL.EventPayload -> Intent
+payloadToIntent SDL.QuitEvent         = Quit
+payloadToIntent (SDL.KeyboardEvent k) = getKey k
+payloadToIntent _                     = Idle
+
+getKey :: SDL.KeyboardEventData -> Intent
+getKey (SDL.KeyboardEventData _ SDL.Released _ _) = Idle
+getKey (SDL.KeyboardEventData _ SDL.Pressed True _) = Idle
+getKey (SDL.KeyboardEventData _ SDL.Pressed False keysym) =
+  case SDL.keysymKeycode keysym of
+    SDL.KeycodeEscape -> Quit
+    SDL.KeycodeUp     -> SelectSurface Up
+    SDL.KeycodeDown   -> SelectSurface Down
+    SDL.KeycodeLeft   -> SelectSurface Left
+    SDL.KeycodeRight  -> SelectSurface Right
+    _                 -> SelectSurface Help
+
+runIntent :: (Monad m) => SurfaceMap a -> (a -> m ()) -> Intent -> m Bool
+runIntent _ _ Quit
+  = pure False
+
+runIntent _ _ Idle
+  = pure True
+
+runIntent cs f (SelectSurface key)
+  = True <$ f (selectSurface key cs)
